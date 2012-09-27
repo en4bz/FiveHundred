6f0fc317215c57c5106f1ce8a28a9c2f2be8d0bf
rkhour17 <rkhour17>
2012-09-27 09:41:02 +0000

    Complete (there is an issue with suit of the contract though) but untested commit

--- src/comp303/fivehundred/ai/RandomPlayingStrategy.java
+++ src/comp303/fivehundred/ai/RandomPlayingStrategy.java
@@ -1,11 +1,41 @@
 package comp303.fivehundred.ai;
 
+import comp303.fivehundred.model.Bid;
 import comp303.fivehundred.model.Hand;
 import comp303.fivehundred.model.Trick;
 import comp303.fivehundred.util.Card;
+import comp303.fivehundred.util.Card.Suit;
+import comp303.fivehundred.util.CardList;
+
+
+
+/* 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * NO ACCESS TO THE SUIT OF THE CONTRACT!
+ * CANNOT FULFILL THE FIRST REQUIREMENT COMPLETELY!
+ * MUST FIX!
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
 
 /**
- * If leading, picks a card at random except a joker if the contract is in no trump.
+ * @author Rayyan Khoury
+ *  * If leading, picks a card at random except a joker if the contract is in no trump.
  * If not leading and the hand contains cards that can follow suit, pick a suit-following 
  * card at random. If not leading and the hand does not contain cards that can follow suit,
  * pick a card at random (including trumps, if available).
@@ -15,6 +45,54 @@ public class RandomPlayingStrategy implements IPlayingStrategy
 	@Override
 	public Card play(Trick pTrick, Hand pHand)
 	{
-		return null;
+		
+		// The Card to return
+		Card chosen = null;
+		
+		// Leading card
+		Card leadingCard = pTrick.cardLed();
+		Suit leadingSuit = leadingCard.getSuit();
+		
+		// Suit of the current play
+		// Suit current = Bid.getSuit();
+		
+		// Checks if this player is leading or not and selects a card at random
+		if (pTrick.size() == 0) 
+		{
+			// While loop chosen in case method returns null
+			while (chosen != null)
+			{
+				chosen = pHand.random();
+			}
+			
+			return chosen;
+			
+		}
+		
+		// Cards that are of the current suit being led
+		CardList playableCards = pHand.getTrumpCards(leadingSuit);
+		
+		// While loop chosen in case method returns null
+		while (chosen != null) 
+		{
+			
+			if (playableCards.size() != 0) 
+			{
+				
+				chosen = playableCards.random();
+				
+			}
+			
+			else 
+			{
+				
+				chosen = pHand.random();
+				
+			}
+			
+		}
+		
+		return chosen;
 	}
+	
 }
