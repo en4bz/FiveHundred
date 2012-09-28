e9ff08776e9d24b332c6708f7d14556bab156384
iforbe <iforbe>
2012-09-28 03:39:48 +0000

    Started

--- tests/comp303/fivehundred/model/TestTrick.java
+++ tests/comp303/fivehundred/model/TestTrick.java
@@ -1,6 +1,17 @@
 package comp303.fivehundred.model;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+import comp303.fivehundred.util.Card.Suit;
+
 public class TestTrick
 {
-
+	@Test
+	public void testGetTrumpSuit()
+	{
+		Trick testTrick = new Trick(new Bid(8,Suit.HEARTS));
+		assertEquals(Suit.HEARTS, testTrick.getTrumpSuit());
+	}
 }
