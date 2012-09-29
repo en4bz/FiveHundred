50fc9a3b1e9150d52bf2aa297016bd8cfb955dc0
iforbe <iforbe>
2012-09-29 18:13:06 +0000

    Cleaned imports.

--- tests/comp303/fivehundred/model/TestBid.java
+++ tests/comp303/fivehundred/model/TestBid.java
@@ -1,7 +1,7 @@
 package comp303.fivehundred.model;
 
-import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
