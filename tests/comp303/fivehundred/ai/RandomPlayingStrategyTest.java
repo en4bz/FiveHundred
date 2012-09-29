8512e5de6c98e2520df8b3a0a476179568f3e9d3
ggerma2 <ggerma2>
2012-09-29 01:48:40 +0000

    RandomPlayingStrategyTest -> 100% completed.

--- tests/comp303/fivehundred/ai/RandomPlayingStrategyTest.java
+++ tests/comp303/fivehundred/ai/RandomPlayingStrategyTest.java
@@ -34,7 +34,6 @@ public class RandomPlayingStrategyTest
 		//Test 1: Leading(empty trick), no trump, hand contains a joker. The method must not return the joker
 		Bid someBid1 = new Bid (6, null); //null = no trump
 		Trick emptyTrick = new Trick (someBid1);
-		emptyTrick.add(AllCards.a5D);
 		
 		Card testCard1 = testStrategy.play(emptyTrick, newHand);
 		assertNotSame(testCard1, AllCards.aLJo); // the card returned must not be the joker
@@ -46,18 +45,18 @@ public class RandomPlayingStrategyTest
 		initiallyEmptyTrick.add(AllCards.a5H); // initiallyEmptyTrick now contains one trick that was played already
 		
 		Card testCard2 = testStrategy.play(initiallyEmptyTrick, newHand);
-		assertEquals(testCard2, AllCards.aJH); //must return the only "heart" that the hand contains, since the suit played is heart
+		assertTrue(testCard2 == AllCards.aJH || testCard2 == AllCards.aLJo); //must return the only "heart" that the hand contains, since the suit played is heart, Can also return a joker
+
 		
-		
-//		//Test 3: Not Leading, trump = Spades, hand does not contain card of the same suit as the tricks played, but contains a spade. Must return trump (spades)
-		Bid someBid3 = new Bid (6, Suit.SPADES);
+		//Test 3: Not Leading, trump=Spades, the hand does not contain cards with the suit played before (here:diamonds). Must return a spade or a joker
+		Bid someBid3 = new Bid (9, null);
 		Trick notEmptyTrick = new Trick (someBid3); // empty trick
 		notEmptyTrick.add(AllCards.a4D); 
 		notEmptyTrick.add(AllCards.a6D); // initiallyEmptyTrick now contains two tricks that were played already
 		
 		Card testCard3 = testStrategy.play(notEmptyTrick, newHand);
 		
-		assertEquals(testCard3, AllCards.a9S);//Return "Spades"
+		assertTrue(testCard3 == AllCards.a9S || testCard3 == AllCards.aLJo); //Return "Spades" or a joker
 		
 		
 		//Test 4: Leading, trump : Can return any card from the hand, joker included (Test if a card is returned)
@@ -66,6 +65,9 @@ public class RandomPlayingStrategyTest
 		
 		Card testCard4 = testStrategy.play(emptyTrick2, newHand);
 		assertNotSame(testCard4, null); //must return a card
+		
+		
+		
 		
 	}
 
