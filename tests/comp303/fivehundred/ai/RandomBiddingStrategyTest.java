75c528025e1f63128081278886a3ca59a6db4be1
ggerma2 <ggerma2>
2012-09-29 02:39:32 +0000

    Test SelectBid -> done. (Two more methods to test)

--- tests/comp303/fivehundred/ai/RandomBiddingStrategyTest.java
+++ tests/comp303/fivehundred/ai/RandomBiddingStrategyTest.java
@@ -4,25 +4,56 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import comp303.fivehundred.model.Bid;
+import comp303.fivehundred.model.Hand;
+import comp303.fivehundred.util.AllCards;
+import comp303.fivehundred.util.GameUtilException;
+
 public class RandomBiddingStrategyTest
 {
 
 	@Test
 	public void testRandomBiddingStrategy()
 	{
-		fail("Not yet implemented");
+		//TODO
+		
 	}
 
 	@Test
 	public void testRandomBiddingStrategyInt()
 	{
-		fail("Not yet implemented");
+		//TODO
 	}
 
 	@Test
 	public void testSelectBid()
 	{
-		fail("Not yet implemented");
+		
+		//new hand
+		Hand newHand = new Hand();
+		newHand.add(AllCards.a4D);
+		newHand.add(AllCards.aJH);
+		newHand.add(AllCards.a7D);
+		newHand.add(AllCards.a8S);
+		
+		//Test 1:Highest bid has already been bid. Must return a new bid
+		
+		Bid[] testBid1 = {new Bid(24), new Bid(15), new Bid(12)}; //new array of bids
+		
+		RandomBiddingStrategy strategyTest1 = new RandomBiddingStrategy();
+		
+		Bid bidSelected1 = strategyTest1.selectBid(testBid1, newHand);
+		assertEquals(bidSelected1, new Bid()); // must return new bid() 
+		
+		
+		//Test 2: The highest bid has not been bid. 
+		
+		Bid[] testBid2 = {new Bid(20), new Bid(15), new Bid(12)}; //new array of bids
+		RandomBiddingStrategy strategyTest2 = new RandomBiddingStrategy();
+		
+		Bid bidSelected2 = strategyTest2.selectBid(testBid2, newHand);
+		
+		assertNotNull(bidSelected2.getTricksBid()); // must return a bid
 	}
 
 }
