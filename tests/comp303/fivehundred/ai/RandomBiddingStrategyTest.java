c7e548970d45b50b6bb9b33c0de4d3ca514a3865
iforbe <iforbe>
2012-09-30 18:27:58 +0000

    Edited testSelectBid as it was throwing an exception because getTricks was being called on a passing bid. I don't know whether this is an error with the test or the class.

--- tests/comp303/fivehundred/ai/RandomBiddingStrategyTest.java
+++ tests/comp303/fivehundred/ai/RandomBiddingStrategyTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import comp303.fivehundred.model.Bid;
 import comp303.fivehundred.model.Hand;
+import comp303.fivehundred.model.ModelException;
 import comp303.fivehundred.util.AllCards;
 import comp303.fivehundred.util.GameUtilException;
 
@@ -30,7 +31,6 @@ public class RandomBiddingStrategyTest
 	@Test
 	public void testSelectBid()
 	{
-		
 		//new hand
 		Hand newHand = new Hand();
 		newHand.add(AllCards.a4D);
@@ -47,17 +47,21 @@ public class RandomBiddingStrategyTest
 		Bid bidSelected1 = strategyTest1.selectBid(testBid1, newHand);
 		assertEquals(bidSelected1, new Bid()); // must return new bid() 
 		
-		
 		//Test 2: The highest bid has not been bid. 
 		
 		Bid[] testBid2 = {new Bid(20), new Bid(15), new Bid(12)}; //new array of bids
 		RandomBiddingStrategy strategyTest2 = new RandomBiddingStrategy();
 		
 		Bid bidSelected2 = strategyTest2.selectBid(testBid2, newHand);
-		
-		//Bid is null if pass; otherwise, does no pass if bid is not null
-		assertTrue(bidSelected2 == null || bidSelected2 != null);
-
+		try
+		{
+			 // must return a bid, but that bid can be a pass which will raise an exception
+			assertNotNull(bidSelected2.getTricksBid());
+		}
+		catch(ModelException e)
+		{
+			
+		}
 	}
 
 }
