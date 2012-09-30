328da743b328e33859287bad076846491cf45cfc
iforbe <iforbe>
2012-09-30 16:45:21 +0000

    *** empty log message ***

--- tests/comp303/fivehundred/util/TestBySuitNoTrumpComparator.java
+++ tests/comp303/fivehundred/util/TestBySuitNoTrumpComparator.java
@@ -1,18 +1,9 @@
 package comp303.fivehundred.util;
 
-import static comp303.fivehundred.util.AllCards.a4D;
-import static comp303.fivehundred.util.AllCards.a4H;
-import static comp303.fivehundred.util.AllCards.a4S;
-import static comp303.fivehundred.util.AllCards.a5S;
-import static comp303.fivehundred.util.AllCards.a8D;
-import static comp303.fivehundred.util.AllCards.a9C;
-import static comp303.fivehundred.util.AllCards.a9D;
-import static comp303.fivehundred.util.AllCards.aHJo;
-import static comp303.fivehundred.util.AllCards.aLJo;
+import static comp303.fivehundred.util.AllCards.*;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-
 import comp303.fivehundred.util.Card.BySuitNoTrumpComparator;
 
 /**
@@ -28,7 +19,7 @@ public class TestBySuitNoTrumpComparator
 	{
 		assertTrue(tester.compare(a4D, a4D) == 0);
 		assertTrue(tester.compare(a4S, a8D) > 0);
-		assertTrue(tester.compare(a9D, a5S) > 0);
+		assertTrue(tester.compare(a9D, a9S) > 0);
 		assertTrue(tester.compare(a4H, a4H) == 0);
 		assertTrue(tester.compare(aHJo, a4D) > 0);
 		assertTrue(tester.compare(a9C, aLJo) < 0);
