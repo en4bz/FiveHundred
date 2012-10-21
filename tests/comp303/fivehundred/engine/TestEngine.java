e91c927bf6b643fd843cec9e2f2d266a2c08dbd4
iforbe <iforbe>
2012-10-21 22:50:49 +0000

    Added test package for engine.

---
+++ tests/comp303/fivehundred/engine/TestEngine.java
@@ -0,0 +1,19 @@
+package comp303.fivehundred.engine;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author Ian Forbes
+ *
+ */
+public class TestEngine
+{
+	GameEngine aTester = new GameEngine(null, null, null, null);
+
+	@Test
+	public void testGame()
+	{
+		
+	}
+}
