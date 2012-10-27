725d32e789887d3b8e6e1cadb9bd9a84f154dc44
ezarou <ezarou>
2012-10-27 16:53:10 +0000

    Changed to RandomRobots for now and only two games played for testing purposes.

--- src/comp303/fivehundred/engine/Driver.java
+++ src/comp303/fivehundred/engine/Driver.java
@@ -1,5 +1,6 @@
 package comp303.fivehundred.engine;
 
+import comp303.fivehundred.ai.BasicRobot;
 import comp303.fivehundred.ai.RandomRobot;
 import comp303.fivehundred.player.Team;
 
@@ -8,7 +9,7 @@ public class Driver
 	
 	public static void main(String[] args)
 	{
-		int MAX_GAMES = 50;
+		int MAX_GAMES = 2;
 		Team lTeam1 = new Team(new RandomRobot("RandomA"), new RandomRobot("RandomB"));
 		Team lTeam2 = new Team(new RandomRobot("BasicA"), new RandomRobot("BasicB"));
 		GameEngine lEngine = new GameEngine(lTeam1, lTeam2);
@@ -25,8 +26,7 @@ public class Driver
 				{
 					lEngine.deal();
 					lEngine.bid();
-				}
-				while (lEngine.allPasses());
+				} while (lEngine.allPasses());
 				
 				lEngine.exchange();
 				lEngine.playRound();
