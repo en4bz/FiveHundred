533b21a53268973d320b09a6b021e98985e1ec1d
iforbe <iforbe>
2012-10-28 15:53:03 +0000

    Initial Commit.

---
+++ tests/comp303/fivehundred/ai/BasicsBiddingStrategyTest.java
@@ -0,0 +1,14 @@
+package comp303.fivehundred.ai;
+
+import org.junit.Test;
+
+public class BasicsBiddingStrategyTest
+{
+	private BasicBiddingStrategy lStratgey = new BasicBiddingStrategy();
+	
+	@Test
+	public void testBid()
+	{
+		
+	}
+}
