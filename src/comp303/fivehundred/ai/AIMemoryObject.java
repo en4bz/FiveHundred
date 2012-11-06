e35703a35f0c1e986a5dcf74445c301698b6406d
rkhour17 <rkhour17>
2012-11-06 07:07:48 +0000

    *** empty log message ***

--- src/comp303/fivehundred/ai/AIMemoryObject.java
+++ src/comp303/fivehundred/ai/AIMemoryObject.java
@@ -1,11 +1,16 @@
 package comp303.fivehundred.ai;
 
+import java.util.Iterator;
+
+import comp303.fivehundred.model.Trick;
+import comp303.fivehundred.util.Card;
 import comp303.fivehundred.util.CardList;
 
 public class AIMemoryObject
 {
 	
 	private static CardList aCardsPlayed;
+	private static Iterator<Card> aIt;
 	
 	public AIMemoryObject ()
 	{
@@ -14,11 +19,41 @@ public class AIMemoryObject
 		
 	}
 	
-	public void newGame ()
+	public void newGame()
 	{
 		
-		aCardsPlayed = new CardList();
+		aCardsPlayed.clear();
 		
 	}
+	
+	public void addCards(Trick pTrick)
+	{
+	
+		aIt = pTrick.iterator();
+		
+		while (aIt.hasNext())
+		{
+			
+			aCardsPlayed.add(aIt.next());
+			
+		}
+		
+	}
+	
+	public boolean alreadyPlayed(Card pCard)
+	{
+		
+		return aCardsPlayed.contains(pCard);
+		
+	}
+	
+	public CardList allCardsPlayed()
+	{
+		
+		return aCardsPlayed.clone();
+		
+	}
+	
+	
 
 }
