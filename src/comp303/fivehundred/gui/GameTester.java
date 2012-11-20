5933cd2212f0f03b20b323d0ab3b3b8ea8ae831d
iforbe <iforbe>
2012-11-20 22:23:33 +0000

    My Testing Class.

---
+++ src/comp303/fivehundred/gui/GameTester.java
@@ -0,0 +1,49 @@
+package comp303.fivehundred.gui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+import comp303.fivehundred.model.AllCards;
+import comp303.fivehundred.util.CardList;
+
+/**
+ * Testing Class for CardListPanel and HandPanel
+ * @author Ian Forbes
+ */
+@SuppressWarnings("serial")
+public class GameTester extends JFrame
+{
+	public GameTester()
+	{
+		setTitle("Five Hundred");
+		setLayout(new BorderLayout());
+		((BorderLayout) this.getLayout()).setHgap(50);
+		((BorderLayout) this.getLayout()).setVgap(50);
+		
+		CardList cList = new CardList();
+		cList.add(AllCards.aHJo);
+		cList.add(AllCards.a4D);
+		cList.add(AllCards.a4H);
+		cList.add(AllCards.aLJo);
+		
+		CardListPanel Derp1 = new CardListPanel(cList,Rotation.ABOUT_CENTER, true);
+		CardListPanel Derp2 = new CardListPanel(cList,Rotation.RIGHT, true);
+		CardListPanel Derp3 = new CardListPanel(cList,Rotation.LEFT, true);
+		CardListPanel Derp4 = new CardListPanel(cList,Rotation.ABOUT_CENTER, true);
+		
+		this.add(Derp1, BorderLayout.PAGE_START);
+		this.add(Derp2, BorderLayout.LINE_START);
+		this.add(Derp3, BorderLayout.LINE_END);
+		this.add(Derp4, BorderLayout.PAGE_END);
+		
+		this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		this.setLocationRelativeTo(null);
+		this.pack();
+		this.setVisible( true );
+	}
+	
+	public static void main(String[] args){
+		new GameTester();
+	}
+}
