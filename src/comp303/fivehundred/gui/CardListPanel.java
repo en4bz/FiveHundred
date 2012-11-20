e4b49ffeca2253a958289758adf895dfcf92bbaa
iforbe <iforbe>
2012-11-20 02:53:10 +0000

    Added Java Docs

--- src/comp303/fivehundred/gui/CardListPanel.java
+++ src/comp303/fivehundred/gui/CardListPanel.java
@@ -4,8 +4,14 @@ import java.awt.Point;
 
 import javax.swing.JPanel;
 
+import comp303.fivehundred.gui.external.OverlapLayout;
 import comp303.fivehundred.util.Card;
 import comp303.fivehundred.util.CardList;
+
+/**
+ * A JPanel that represents a list of overlapped cards.
+ * @author Ian Forbes
+ */
 
 @SuppressWarnings("serial")
 public class CardListPanel extends JPanel
@@ -14,6 +20,12 @@ public class CardListPanel extends JPanel
 	private Rotation aRotation;
 	private boolean aIsVisible;
 	
+	/**
+	 * Creates a new CardList Panel
+	 * @param pCardList : CardList to display
+	 * @param pRotation : Rotation of the list on the display
+	 * @param pVisibility : Whether the card is facing up
+	 */
 	public CardListPanel(CardList pCardList, Rotation pRotation, boolean pVisibility)
 	{
 		super();
@@ -38,21 +50,4 @@ public class CardListPanel extends JPanel
 			this.add(new CardLabel(c,aRotation, aIsVisible));
 		}
 	}
-
-	
-//	@Override
-//	public void paintComponent(Graphics pGraphic)
-//	{
-//	    Graphics2D g = (Graphics2D) pGraphic;
-//	 //   g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-//	 //   AffineTransform matrix = g.getTransform(); // Backup
-//	    g.rotate(aRotation, this.getWidth() / 2, this.getHeight() / 2);
-//
-//	    this.paint(g);
-//	    
-//	    /* End */
-//	  //  g.setTransform(matrix); // Restore
-//
-//	}
-	
 }
