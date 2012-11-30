e48e3438ed0810b77885f8667388d9bc6f75a2d6
ezarou <ezarou>
2012-11-30 17:56:46 +0000

    GameFrame now integrates PlayerMenu such that the GameBoard is built only after the user hits "Play".

--- src/comp303/fivehundred/gui/GameFrame.java
+++ src/comp303/fivehundred/gui/GameFrame.java
@@ -1,11 +1,18 @@
 package comp303.fivehundred.gui;
 
 import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ResourceBundle;
 import java.awt.FlowLayout;
 
 import javax.management.Notification;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
+import javax.swing.JPanel;
 
 import comp303.fivehundred.engine.GameEngine;
 import comp303.fivehundred.mvc.Observer;
@@ -19,23 +26,33 @@ public class GameFrame extends JFrame implements Observer
 	private long aNotificationSequenceNumber = 0;
 	private GameEngine aEngine;
 	private PlayerMenu aPlayerMenu;
+	private GameBoard aBoard;
+	public static int WIDTH = 900;
+	public static int HEIGHT = 600;
 	
-	private boolean aPracticeModeOn;
+	private boolean aPracticeModeOn = true;
 
 	public GameFrame()
 	{
-//		aPlayerMenu = new PlayerMenu(this);
+		// Build basic frame
 		this.setTitle("Five Hundred");	
-
+		this.setLayout(new FlowLayout());
+		this.setLocation(0, 0); //Top-left corner of the screen
+		this.setSize(new Dimension(WIDTH, HEIGHT));
+		this.setResizable(false); //We cannot change the dimensions of the window by hand when the program runs (components don't move)	
+		
+		// Add Menu
 		this.setJMenuBar(new Menu()); //add menu to the new window (new game)
 		
-		this.setLayout(new FlowLayout());
-		this.add(new GameBoard());
-		this.setLocation(0, 0); //Top-left corner of the screen
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Program ends when we close the window
-		this.setVisible(true); //Makes it visible
-		this.pack(); //minimal size to see all components
-		this.setResizable(false); //We cannot change the dimensions of the window by hand when the program runs (components don't move)	
+		// Add panel to configure game settings
+		aPlayerMenu = new PlayerMenu(this);
+		this.add(aPlayerMenu);
+				
+		// Make visible
+		this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		this.setLocationRelativeTo(null);
+		this.pack();
+		this.setVisible( true );
 	}
 	
 	public static void main(String[] args)
@@ -53,13 +70,33 @@ public class GameFrame extends JFrame implements Observer
 			{
 				case newGame:
 					Team[] lTeams = aPlayerMenu.getTeams();
+					System.out.println(lTeams[0]);
+					System.out.println(lTeams[1]);
 					aEngine = new GameEngine(lTeams[0], lTeams[1]);
 					aPracticeModeOn = aPlayerMenu.isPracticeModeOn();
+					this.remove(aPlayerMenu);
+					aBoard = new GameBoard(); // TODO pass frame
+					this.add(aBoard);
+					this.pack();
+					
+//					JPanel contentPane = (JPanel) getContentPane();
+//					contentPane.removeAll();
+//					setTitle("Five Hundred");
+//					this.getContentPane().invalidate(); 
+//					this.getContentPane().repaint();
 					break;
+				case newDeal:
+					// TODO redraw handpanels Hand.update(
+					break;
+				
 				default:
 					break;
 			}
 		}
+	}
+	
+	GameEngine getGameEngine() {
+		return aEngine;
 	}
 			
 	protected long getNotificationSequenceNumber()
@@ -68,7 +105,16 @@ public class GameFrame extends JFrame implements Observer
 	}
 	
 	public enum State {
-		newGame
+		newGame,
+		newDeal,
+		newBid,
+		newContract,
+		cardsDiscarded,
+		cardPlayed,
+		newTrick,
+		trickWon,
+		roundEnd,
+		gameOver
 	}
 
 }
