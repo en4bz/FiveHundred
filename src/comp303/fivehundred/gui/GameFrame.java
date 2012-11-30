71711d8502e314232ef7c9358a9d0bb0fd6f5d7c
iforbe <iforbe>
2012-11-30 02:58:00 +0000

    This is now the official JFrame class.

--- src/comp303/fivehundred/gui/GameFrame.java
+++ src/comp303/fivehundred/gui/GameFrame.java
@@ -1,28 +1,21 @@
 package comp303.fivehundred.gui;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ResourceBundle;
+import java.awt.FlowLayout;
 
 import javax.management.Notification;
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JSlider;
+import javax.swing.JMenuBar;
 
-import comp303.fivehundred.util.Card;
 import comp303.fivehundred.engine.GameEngine;
 import comp303.fivehundred.mvc.Observer;
-import comp303.fivehundred.player.*;
+import comp303.fivehundred.player.Team;
 
 @SuppressWarnings("serial")
 public class GameFrame extends JFrame implements Observer
 {
 	public static Color BACKGROUND_COLOR = new Color(40, 160, 20);
-	private static final ResourceBundle MESSAGES = ResourceBundle.getBundle("comp303.fivehundred.gui.MessageBundle");
+//	private static final ResourceBundle MESSAGES = ResourceBundle.getBundle("comp303.fivehundred.gui.MessageBundle");
 	private long aNotificationSequenceNumber = 0;
 	private GameEngine aEngine;
 	private PlayerMenu aPlayerMenu;
@@ -31,7 +24,18 @@ public class GameFrame extends JFrame implements Observer
 
 	public GameFrame()
 	{
-		aPlayerMenu = new PlayerMenu(this);
+//		aPlayerMenu = new PlayerMenu(this);
+		this.setTitle("Five Hundred");	
+
+		this.setJMenuBar(new Menu()); //add menu to the new window (new game)
+		
+		this.setLayout(new FlowLayout());
+		this.add(new GameBoard());
+		this.setLocation(0, 0); //Top-left corner of the screen
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Program ends when we close the window
+		this.setVisible(true); //Makes it visible
+		this.pack(); //minimal size to see all components
+		this.setResizable(false); //We cannot change the dimensions of the window by hand when the program runs (components don't move)	
 	}
 	
 	public static void main(String[] args)
