1a0c98f398bf85291a8b69253b523f10b1de06c3
iforbe <iforbe>
2012-12-01 22:00:08 +0000

    Initial

---
+++ src/comp303/fivehundred/gui/TrickPanel.java
@@ -0,0 +1,67 @@
+package comp303.fivehundred.gui;
+
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import comp303.fivehundred.util.Card;
+
+public class TrickPanel extends JPanel
+{
+	private Card aNorth;
+	private Card aSouth;
+	private Card aEast;
+	private Card aWest;
+	
+	JPanel aNorthSouth;
+	
+	public TrickPanel(){
+		super(new FlowLayout());
+		aNorthSouth = new JPanel(new GridLayout(2,1));
+		this.redraw();
+	}
+	
+	private void redraw(){
+		this.removeAll();
+		this.add(new CardLabel(aEast,Rotation.ABOUT_CENTER, true));
+		aNorthSouth.removeAll();
+		aNorthSouth.add(new CardLabel(aSouth,Rotation.ABOUT_CENTER,true));
+		aNorthSouth.add(new CardLabel(aNorth,Rotation.ABOUT_CENTER,true));
+		this.add(aNorthSouth);
+		this.add(new CardLabel(aWest,Rotation.ABOUT_CENTER, true));
+
+		this.validate();
+		this.repaint();
+	}
+	
+	public void setNorth(Card pCard){
+		aNorth = pCard;
+		this.redraw();
+	}
+	
+	public void setSouth(Card pCard){
+		aSouth = pCard;
+		this.redraw();
+	}
+	
+	public void setEast(Card pCard){
+		aEast = pCard;
+		this.redraw();
+	}
+	
+	public void setWest(Card pCard){
+		aWest = pCard;
+		this.redraw();
+	}
+	
+	public void reset(){
+		aNorth = null;
+		aSouth = null;
+		aEast = null;
+		aWest = null;
+		this.redraw();
+	}
+}
+
