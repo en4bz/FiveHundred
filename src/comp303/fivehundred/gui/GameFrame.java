e2b40955b28e172583a861fac060d73c0a825c98
ezarou <ezarou>
2012-12-02 04:18:09 +0000

    merged main thread with event dispatcher

--- src/comp303/fivehundred/gui/GameFrame.java
+++ src/comp303/fivehundred/gui/GameFrame.java
@@ -3,12 +3,19 @@ package comp303.fivehundred.gui;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ResourceBundle;
 
 import javax.management.Notification;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import comp303.fivehundred.engine.GameEngine;
+import comp303.fivehundred.engine.GameLogger;
 import comp303.fivehundred.engine.GameStatistics;
 import comp303.fivehundred.mvc.Observer;
 import comp303.fivehundred.player.Team;
@@ -16,174 +23,252 @@ import comp303.fivehundred.player.Team;
 @SuppressWarnings("serial")
 public class GameFrame extends JFrame implements Observer
 {
-	public static Color BACKGROUND_COLOR = new Color(40, 160, 20);
-	private static final ResourceBundle MESSAGES = ResourceBundle.getBundle("comp303.fivehundred.gui.MessageBundle");
-	private long aNotificationSequenceNumber = 0;
-	private static GameEngine aEngine;
-	private PlayerMenu aPlayerMenu;
-	private GameBoard aBoard;
-	public static int WIDTH = 900;
-	public static int HEIGHT = 600;
-	
-	private boolean aPracticeModeOn = true;
+    public static Color BACKGROUND_COLOR = new Color(40, 160, 20);
+    private static final ResourceBundle MESSAGES = ResourceBundle.getBundle("comp303.fivehundred.gui.MessageBundle");
+    private long aNotificationSequenceNumber = 0;
+    private GameEngine aEngine;
+    private GameStatistics aGameStats;
+    private GameLogger aGameLogger;
+    private PlayerMenu aPlayerMenu;
+    private GameBoard aBoard;
+    public static int WIDTH = 900;
+    public static int HEIGHT = 600;
+    private int aGamesLeft = 10;
+    private boolean aPracticeModeOn = true;
+    
+    private final static Logger aLogger = LoggerFactory.getLogger("GameFrameLogger");
+    private static boolean aIsLogging = true;
 
-	public GameFrame()
-	{
-		// Build basic frame
-		this.setTitle("Five Hundred");	
-		this.setLayout(new FlowLayout());
-		this.setLocation(0, 0); //Top-left corner of the screen
-		this.setSize(new Dimension(WIDTH, HEIGHT));
-		this.setResizable(false); //We cannot change the dimensions of the window by hand when the program runs (components don't move)	
-		
-		// Add Menu
-		this.setJMenuBar(new Menu()); //add menu to the new window (new game)
-		
-		// Add panel to configure game settings
-		aPlayerMenu = new PlayerMenu(this);
-		this.add(aPlayerMenu);
-				
-		// Make visible
-		this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-		this.setLocationRelativeTo(null);
-		this.pack();
-		this.setVisible( true );
-	}
-	
-	public static void main(String[] args)
-	{
-		GameFrame a = new GameFrame();
-		try
-		{
-			Thread.sleep(5000);
-		}
-		catch (InterruptedException e){}
-		
-		GameStatistics lGameStats = new GameStatistics();
-		aEngine.addObserver(lGameStats);
-		aEngine.addObserver(a);
-		// TO TURN LOGGING ON UNCOMMENT THIS LINE
-		// lEngine.addObserver(new GameLogger());
-		
-		for(int i = 0; i < 10; i++)
-		{
-			aEngine.newGame();
-			while(!aEngine.isGameOver())
-			{
-				do
-				{
-					aEngine.deal();
-					aEngine.bid();
-				} while (aEngine.allPasses());
-				aEngine.exchange();
-				aEngine.playRound();
-				try
-				{
-					Thread.sleep(500);
-				}
-				catch (InterruptedException e){}
-				aEngine.computeScore();
-			}
-		}
-		lGameStats.printStatistics();
-	}
+    public GameFrame()
+    {
+        // Build basic frame
+        this.setTitle("Five Hundred");  
+        this.setLayout(new FlowLayout());
+        this.setLocation(0, 0); //Top-left corner of the screen
+        this.setSize(new Dimension(WIDTH, HEIGHT));
+        this.setResizable(false); //We cannot change the dimensions of the window by hand when the program runs (components don't move) 
+        
+        // Add Menu
+        this.setJMenuBar(new Menu()); //add menu to the new window (new game)
+        
+        // Start the party
+        aPlayerMenu = new PlayerMenu(this);
+        add(aPlayerMenu);
+        
+        // Make visible
+        this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        this.setLocationRelativeTo(null);
+        this.pack();
+        this.setVisible( true );
+        
+    }
+    
+    public static void main(String[] args)
+    {
+        GameFrame a = new GameFrame();
+    }
 
-	@Override
-	public void update(Notification pNotification)
-	{
-		if(pNotification.getType().equals("gui.gameframe"))
-		{
-			State lState = State.valueOf(pNotification.getMessage());
-			switch (lState) 
-			{
-				case newGame:
-					Team[] lTeams = aPlayerMenu.getTeams();
-					System.out.println(lTeams[0]);
-					System.out.println(lTeams[1]);
-					aEngine = new GameEngine(lTeams[0], lTeams[1]); //Shouldn't make new game engine every time
-										aEngine.deal();
-					aPracticeModeOn = aPlayerMenu.isPracticeModeOn();
-					this.remove(aPlayerMenu);
-					aBoard = new GameBoard(lTeams); 
+    
+    private void newGameSet()
+    {
+    	log("Starting new game set.");
+        Team[] lTeams = aPlayerMenu.getTeams();
+        System.out.println(lTeams[0]);
+        System.out.println(lTeams[1]);
+        
+        aPracticeModeOn = aPlayerMenu.isPracticeModeOn();
+        aGamesLeft = aPlayerMenu.getNumberOfGamesToPlay();
+        
+        // make new game engine
+        aEngine = new GameEngine(lTeams[0], lTeams[1]);
+        
+        remove(aPlayerMenu);
+        aBoard = new GameBoard(lTeams); 
+        add(aBoard);
+        pack();
+        invalidate();
+        validate();
+        repaint();
+        log("Game Board drawn.");
+        
+        log("Adding Observers.");
+        aEngine.addObserver(this);
+        aGameStats = new GameStatistics();
+        aGameLogger = new GameLogger();
+        aEngine.addObserver(aGameStats);
+        aEngine.addObserver(aGameLogger);
 
-					this.add(aBoard);
-					this.pack();
-					
-//					JPanel contentPane = (JPanel) getContentPane();
-//					contentPane.removeAll();
-//					this.getContentPane().invalidate(); 
-//					this.getContentPane().repaint();
-					break;
-				case newDeal:
-					
-					break;
-				
-				default:
-					
-					break;
-			}
-		}
-		if(pNotification.getType().equals("game.engine")){
-			GameEngine lEngine = (GameEngine) (pNotification.getSource());
-			switch(State.valueOf(pNotification.getMessage())){
-			case newGame:
-				aBoard.resetScores();
-				break;
-			case newDeal:	
-				aBoard.updateCardPanels();
-				aBoard.resetTricksCount();
-				aBoard.updateWidow(lEngine.getWidow());
-				break;
-			case newBid:
-				break;
-			case newContract:
-				aBoard.updateBid(lEngine.getContract());
-				break;
-			case cardsDiscarded:
-				aBoard.updateCardPanel(lEngine.getCurrentPlayer());
-				break;
-			case cardPlayed:
-				aBoard.updateCardPanel(lEngine.getCurrentPlayer());
-				aBoard.updateTrick(lEngine.getCurrentPlayer() ,((GameEngine)pNotification.getSource()).getCardPlayed());
-				break;
-			case newTrick:
-				aBoard.resetCurrentTrick();
-				aBoard.updateTrick(lEngine.getCurrentPlayer() ,((GameEngine)pNotification.getSource()).getCardPlayed());
-				break;
-			case trickWon:
-				aBoard.updateTrickCount(lEngine.getTrickWinner());
-				aBoard.resetCurrentTrick();
-				break;
-			case roundEnd:
-				aBoard.updateScores();
-				break;
-			case gameOver:
-			default:
-				break;
-			}
-		}
-	}
-	
-	GameEngine getGameEngine() {
-		return aEngine;
-	}
-			
-	protected long getNotificationSequenceNumber()
-	{
-		return aNotificationSequenceNumber++;	
-	}
-	
-	public enum State {
-		newGame,
-		newDeal,
-		newBid,
-		newContract,
-		cardsDiscarded,
-		cardPlayed,
-		newTrick,
-		trickWon,
-		roundEnd,
-		gameOver
-	}
+
+        newGame();
+    }
+    
+    private void newGame()
+    {
+        if(aGamesLeft > 0)
+        {
+            log("Starting new game.");
+            aEngine.newGame();
+            newDeal();
+        }
+        else
+        {
+            log("Game Over.");
+            aGameStats.printStatistics();
+            remove(aBoard);
+            JOptionPane.showMessageDialog(this, MESSAGES.getString("comp303.fivehundred.gui.GameFrame.GameSetOver"));
+            aPlayerMenu = new PlayerMenu(this);
+            pack();
+        }
+    }
+    
+    private void newDeal()
+    {
+        log("Starting new deal.");
+        aEngine.deal();
+        newBid();
+    }
+    
+    private void newBid()
+    {
+        log("Starting new bid.");
+        aEngine.bid();
+    }
+    
+    private void play()
+    {
+        log("Play!");
+        aEngine.exchange();
+        aEngine.playRound();
+        aEngine.computeScore();
+        if(aEngine.isGameOver())
+        {
+            aGamesLeft--;
+            newGame();
+        }
+        else
+        {
+            newDeal();
+        }
+    }
+    
+    @Override
+    public void update(Notification pNotification)
+    {
+        if(pNotification.getType().equals("gui.gameframe"))
+        {
+            State lState = State.valueOf(pNotification.getMessage());
+            switch (lState) 
+            {
+                case newGameSet:
+                	Thread t = new Thread(new Runnable() {
+                	    @Override
+                	    public void run() {
+                	       newGameSet();
+                	    }
+                	   });
+                	t.start();
+                	pack();
+                    break; 
+                default:
+                    break;
+            }
+        }
+        if(pNotification.getType().equals("game.engine")){
+            setTitle(pNotification.getMessage());
+            invalidate();
+            validate();
+            repaint();
+            pack();
+            sleep();
+
+ 
+            GameEngine lEngine = (GameEngine) (pNotification.getSource());
+            switch(GameEngine.State.valueOf(pNotification.getMessage())){
+            case newGame:
+                log("Resetting game board scores.");
+                aBoard.resetScores();
+                break;
+            case newDeal: 
+                log("Updating card panels.");
+                aBoard.updateCardPanels();
+                log("Resetting tricks count.");
+                aBoard.resetTricksCount();
+                log("Updating widow.");
+                aBoard.updateWidow(lEngine.getWidow());
+                break;
+            case allPasses:
+            	log("All passes!");
+            	newDeal();
+            	break;
+            case newContract:
+            	log("New contract.");
+                aBoard.updateBid(lEngine.getContract());
+            	play();
+            	break;
+            case cardsDiscarded:
+                aBoard.updateCardPanel(lEngine.getCurrentPlayer());
+                break;
+            case cardPlayed:
+                aBoard.updateCardPanel(lEngine.getCurrentPlayer());
+                aBoard.updateTrick(lEngine.getCurrentPlayer() ,((GameEngine)pNotification.getSource()).getCardPlayed());
+                break;
+            case newTrick:
+                aBoard.resetCurrentTrick();
+                aBoard.updateTrick(lEngine.getCurrentPlayer() ,((GameEngine)pNotification.getSource()).getCardPlayed());
+                break;
+            case trickWon:
+                aBoard.updateTrickCount(lEngine.getTrickWinner());
+                aBoard.resetCurrentTrick();
+                break;
+            case roundEnd:
+                aBoard.updateScores();
+                break;
+            case gameOver:
+                break;
+            default:
+                break;
+            }
+        }
+    }
+    
+    private void sleep()
+    {
+        try
+        {
+            Thread.sleep(500);
+        }
+        catch (InterruptedException e){}
+    }
+    
+    public static void log(String message)
+    {
+        if(aIsLogging)
+            aLogger.info(message);
+    }
+    
+    GameEngine getGameEngine() {
+        return aEngine;
+    }
+            
+    protected long getNotificationSequenceNumber()
+    {
+        return aNotificationSequenceNumber++;   
+    }
+    
+    public enum State {
+        displayMenu,
+        play,
+        newGameSet,
+        newGame,
+        newDeal,
+        newBid,
+        newContract,
+        cardsDiscarded,
+        cardPlayed,
+        newTrick,
+        trickWon,
+        roundEnd,
+        gameOver
+    }
 
 }
