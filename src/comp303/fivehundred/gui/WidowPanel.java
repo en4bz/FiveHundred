c8c1f767447388ed4cce97e9888b25a0b5f5a1c7
iforbe <iforbe>
2012-12-02 19:24:29 +0000

    Panel to display the widow.

---
+++ src/comp303/fivehundred/gui/WidowPanel.java
@@ -0,0 +1,36 @@
+package comp303.fivehundred.gui;
+
+import java.awt.GridLayout;
+
+import javax.swing.JPanel;
+
+import comp303.fivehundred.util.Card;
+import comp303.fivehundred.util.CardList;
+
+@SuppressWarnings("serial")
+public class WidowPanel extends JPanel
+{
+	private CardList aWidowCards;
+	
+	public WidowPanel(){
+		super((new GridLayout(2,3,5,5)));
+		this.setOpaque(false);
+		for(int i = 0; i < 6; i++){
+			this.add(new CardLabel(null,Rotation.DEFAULT,false));
+		}
+	}
+	
+	public void updateWidow(CardList pWidow){
+		aWidowCards = pWidow;
+		this.redraw();
+	}
+	
+	private void redraw(){
+		this.removeAll();
+		for(Card c : aWidowCards){
+				this.add(new CardLabel(c,Rotation.DEFAULT,true));
+		}
+		this.validate();
+		this.repaint();
+	}
+}
