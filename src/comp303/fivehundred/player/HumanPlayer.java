91637b8aeec45cfeaec60f368dd268a07790fdc6
ezarou <ezarou>
2012-12-03 07:41:07 +0000

    index

--- src/comp303/fivehundred/player/HumanPlayer.java
+++ src/comp303/fivehundred/player/HumanPlayer.java
@@ -1,6 +1,5 @@
 package comp303.fivehundred.player;
 
-
 import java.util.Arrays;
 
 import javax.management.Notification;
@@ -14,10 +13,10 @@ import comp303.fivehundred.util.CardList;
 import comp303.fivehundred.util.Card.Joker;
 
 /**
- * Collects methods necessary for human player, must use a UserInterface object.
- * Experimental for now.
+ * Collects methods necessary for human player, must use a UserInterface object. Experimental for now.
+ * 
  * @author Eleyine
- *
+ * 
  */
 public class HumanPlayer extends APlayer
 {
@@ -25,26 +24,24 @@ public class HumanPlayer extends APlayer
 	CardList aPlayable = null;
 	CardList aDiscardable = null;
 	Trick aTrick = null;
-	
+	int aIndex = 0;
 	Bid[] aPreviousBids = null;
-	
+
 	/**
 	 * Constructs a new human player.
-	 * @param pName : Player name
-	 * @param pFrame: the GUI controller
+	 * 
+	 * @param pName
+	 *            : Player name
+	 * @param pFrame
+	 *            : the GUI controller
 	 */
 	public HumanPlayer(String pName, GameFrame pFrame)
 	{
 		super(pName);
 		aFrame = pFrame;
 		aPlayable = getHand();
-	}	
-	
-	public Trick getTrick()
-	{
-		return aTrick;
 	}
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
@@ -52,32 +49,32 @@ public class HumanPlayer extends APlayer
 	{
 		Card lPlayedCard;
 		aTrick = pTrick;
-						
+
 		// Player is leading
-		if(pTrick.size() == 0)
+		if (pTrick.size() == 0)
 		{
 			aPlayable = getHand();
-			
+
 			// Players cannot lead a joker when playing in no trump, except if they have no other cards left.
-			if(pTrick.getTrumpSuit() == null)
+			if (pTrick.getTrumpSuit() == null)
 			{
 				boolean onlyJokersLeft = true;
-				for(Card c: aPlayable)
+				for (Card c : aPlayable)
 				{
-					if(!c.isJoker())
+					if (!c.isJoker())
 					{
 						onlyJokersLeft = false;
 						break;
 					}
 				}
-				if(!onlyJokersLeft)
+				if (!onlyJokersLeft)
 				{
 					aPlayable.remove(new Card(Joker.LOW));
 					aPlayable.remove(new Card(Joker.HIGH));
 				}
 			}
 		}
-		else if(pTrick.getFirst().isJoker())
+		else if (pTrick.getFirst().isJoker())
 		{
 			aPlayable = getHand();
 		}
@@ -85,13 +82,15 @@ public class HumanPlayer extends APlayer
 		{
 			aPlayable = getHand().playableCards(pTrick.getSuitLed(), pTrick.getTrumpSuit());
 		}
-		
-		while(aFrame.getPlayedCard() == null)
+
+		while (aFrame.getPlayedCard() == null)
 		{
 			try
 			{
-				aFrame.update(new Notification("gui.humanplayer", this, aFrame.getNotificationSequenceNumber(), GameFrame.Human.playPrompt.toString()));
-				synchronized(this)
+				System.out.println("PlayedCard == null");
+				aFrame.update(new Notification("gui.humanplayer", this, aFrame.getNotificationSequenceNumber(),
+						GameFrame.Human.playPrompt.toString()));
+				synchronized (this)
 				{
 					this.wait();
 				}
@@ -103,14 +102,10 @@ public class HumanPlayer extends APlayer
 		}
 		lPlayedCard = aFrame.getPlayedCard();
 		removeCardFromHand(lPlayedCard);
-		aFrame.update(new Notification("gui.humanplayer", this, aFrame.getNotificationSequenceNumber(), GameFrame.Human.playDone.toString()));
-		
+		aFrame.update(new Notification("gui.humanplayer", this, aFrame.getNotificationSequenceNumber(),
+				GameFrame.Human.playDone.toString()));
+
 		return lPlayedCard;
-	}
-	
-	public CardList getPlayableCards()
-	{
-		return aPlayable.clone();
 	}
 
 	/**
@@ -121,13 +116,14 @@ public class HumanPlayer extends APlayer
 		assert pPreviousBids.length <= 4;
 		Bid lSelectedBid;
 		aPreviousBids = pPreviousBids;
-		
-		while(aFrame.getSelectedBid() == null)
+
+		while (aFrame.getSelectedBid() == null)
 		{
 			try
 			{
-				aFrame.update(new Notification("gui.humanplayer", this, aFrame.getNotificationSequenceNumber(), GameFrame.Human.bidPrompt.toString()));
-				synchronized(this)
+				aFrame.update(new Notification("gui.humanplayer", this, aFrame.getNotificationSequenceNumber(),
+						GameFrame.Human.bidPrompt.toString()));
+				synchronized (this)
 				{
 					this.wait();
 				}
@@ -138,44 +134,36 @@ public class HumanPlayer extends APlayer
 			}
 		}
 		lSelectedBid = aFrame.getSelectedBid();
-		aFrame.update(new Notification("gui.humanplayer", this, aFrame.getNotificationSequenceNumber(), GameFrame.Human.bidDone.toString()));
+		aFrame.update(new Notification("gui.humanplayer", this, aFrame.getNotificationSequenceNumber(),
+				GameFrame.Human.bidDone.toString()));
 		return lSelectedBid;
-	} 
+	}
 
-	public Bid[] getPreviousBids()
-	{
-		return Arrays.copyOf(aPreviousBids, aPreviousBids.length);
-	}
-	
-	public CardList getDiscardable()
-	{
-		return aDiscardable;
-	}
-	
-	/**
-	 * {@inheritDoc}
-	 */
 	public CardList exchange(Bid[] pBids, int pIndex, Hand pWidow)
 	{
+		aPreviousBids = pBids;
+		aIndex = pIndex;
+
 		CardList toDiscard = null;
 		aDiscardable = new CardList();
-		
-		for(Card c: pWidow)
+
+		for (Card c : pWidow)
 		{
 			aDiscardable.add(c);
 		}
-		for(Card c: pWidow)
+		for (Card c : pWidow)
 		{
 			addCardToHand(c);
 		}
 		aDiscardable = getHand().clone();
-		
-		while(!aFrame.isDiscardDone())
+
+		while (!aFrame.isDiscardDone())
 		{
 			try
 			{
-				aFrame.update(new Notification("gui.humanplayer", this, aFrame.getNotificationSequenceNumber(), GameFrame.Human.discardPrompt.toString()));
-				synchronized(this)
+				aFrame.update(new Notification("gui.humanplayer", this, aFrame.getNotificationSequenceNumber(),
+						GameFrame.Human.discardPrompt.toString()));
+				synchronized (this)
 				{
 					this.wait();
 				}
@@ -186,13 +174,46 @@ public class HumanPlayer extends APlayer
 			}
 		}
 		toDiscard = aFrame.getDiscardedCards();
-		
-		for(Card c: toDiscard)
+
+		for (Card c : toDiscard)
 		{
 			this.removeCardFromHand(c);
 		}
-		aFrame.update(new Notification("gui.humanplayer", this, aFrame.getNotificationSequenceNumber(), GameFrame.Human.discardDone.toString()));
+		aFrame.update(new Notification("gui.humanplayer", this, aFrame.getNotificationSequenceNumber(),
+				GameFrame.Human.discardDone.toString()));
 
 		return toDiscard;
 	}
+
+	/*------------------------------------- Getters --------------------------------------------*/
+
+	public CardList getPlayableCards()
+	{
+		return aPlayable.clone();
+	}
+
+	public Trick getTrick()
+	{
+		return aTrick;
+	}
+
+	public Bid[] getPreviousBids()
+	{
+		return Arrays.copyOf(aPreviousBids, aPreviousBids.length);
+	}
+
+	public int getIndex()
+	{
+		return aIndex;
+	}
+
+	public CardList getDiscardable()
+	{
+		return aDiscardable;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+
 }
