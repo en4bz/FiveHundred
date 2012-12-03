78d817012477f440c76cfb2b60bbfd4927935c67
ezarou <ezarou>
2012-12-03 11:02:22 +0000

    driver now has advanced robots

--- src/comp303/fivehundred/engine/Driver.java
+++ src/comp303/fivehundred/engine/Driver.java
@@ -1,5 +1,6 @@
 package comp303.fivehundred.engine;
 
+import comp303.fivehundred.ai.AdvancedRobot;
 import comp303.fivehundred.ai.BasicRobot;
 import comp303.fivehundred.ai.RandomRobot;
 import comp303.fivehundred.player.Team;
@@ -24,7 +25,7 @@ final class Driver
 	 */
 	public static void main(String[] pArguments)
 	{
-		Team lTeam1 = new Team(new RandomRobot("RandomA"), new RandomRobot("RandomB"));
+		Team lTeam1 = new Team(new AdvancedRobot("AdvancedA"), new AdvancedRobot("AdvancedB"));
 		Team lTeam2 = new Team(new BasicRobot("BasicA"), new BasicRobot("BasicB"));
 		GameEngine lEngine = new GameEngine(lTeam1, lTeam2);
 		GameStatistics lGameStats = new GameStatistics();
